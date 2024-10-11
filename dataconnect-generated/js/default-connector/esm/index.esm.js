8a1a2d365c475a7fbab38875534c9ada073355b8
Ed <silva.email@yahoo.com>
2024-10-11 11:09:15 -0500

    redeployed using --force

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'Shopping List v4.0 2024',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { getDataConnect, validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'Shopping List v4.0 2024',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
